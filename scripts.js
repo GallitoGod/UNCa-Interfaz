2877075ce3e20e1648a44c3f28aa7c6a884fa508
Gallo Pablo <gallopabloj@gmail.com>
2024-12-03 19:03:48 -0300

    Prueba de cambio de camara

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
 })
 const { ipcRenderer } = require('electron');
-
+/*
 document.getElementById('upload-btn').addEventListener('click', () => {
   ipcRenderer.send('load-image');
 });
@@ -15,15 +15,26 @@ ipcRenderer.on('image-loaded', (event, { imagePath, imageBuffer }) => {
 ipcRenderer.on('image-load-cancelled', () => {
   console.log('La carga de imagen fue cancelada');
 });
-
+*/
 
 async function startCamera() {
   const imagePreview = document.getElementById('image-preview');
   const video = document.getElementById('video');
 
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+  console.log('Cámaras disponibles:', videoDevices);
+
+  if (videoDevices.length === 0) {
+    console.error('No se encontraron cámaras disponibles');
+    return;
+  }
+  
   const constraints = {
     audio: false,
     video: {
+      deviceId: videoDevices[0].deviceId,
       width: imagePreview.clientWidth,
       height: imagePreview.clientHeight,
       frameRate: 30
