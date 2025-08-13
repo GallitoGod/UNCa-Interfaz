9514503463613b3e514f447d6d4dbcd271ec4d26
Pablo Gallo <gallopabloj@gmail.com>
2025-08-13 19:29:13 -0300

    Funcionamiento total de tests en deteccion

--- src/api/func/tests/testing_images/script_de_desecho.py
+++ src/api/func/tests/testing_images/script_de_desecho.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 width = 1920
 height = 1080
-img_path = Path(__file__).parent / 'horses_copy.jpg'  # <-- unión correcta
-img = cv2.imread(str(img_path))  # convertir a str para OpenCV
+img_path = Path(__file__).parent / 'horses_copy.jpg' 
+img = cv2.imread(str(img_path))  
 img_resized = cv2.resize(img, (width, height))
 cv2.imwrite('imagen_redimensionada.jpg', img_resized)
